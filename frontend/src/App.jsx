541fe5f49a5416b1e4bf495328f31fe5cc13c028
saiteja-in <vurukondasaiteja13@gmail.com>
2024-09-09 23:46:48 +0530

    react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import SignUp from "./pages/SignUp";
+import { Route, Routes } from "react-router-dom";
+import Header from "./pages/Header";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import TransactionPage from "./pages/TransactionPage";
 
 const App = () => {
+  const authUser = true;
   return (
-    <div className='text-red-500'>
-      Hello
-    </div>
-  )
-}
+    <>
+    {/* {authUser && <Header />} */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/transaction/:id" element={<TransactionPage/>}/> 
+        {/* <Route path="/*" element={<NotFound/>}/> */}
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import Background from "./components/background.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Background>
+        <App />
+      </Background>
+    </BrowserRouter>
+  </StrictMode>
+);
