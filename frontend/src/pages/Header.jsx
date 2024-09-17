15bd942cfce0ff397d9a7fa3bd3e88b2b3d9060a
saiteja-in <vurukondasaiteja13@gmail.com>
2024-09-17 21:48:40 +0530

    fix

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -3,12 +3,10 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<div className=''>
-			<h1 className='md:text-3xl text-3xl lg:text-3xl relative z-50 text-white pt-5 pl-5'>
-				{/* Updated to link only the icon */}
-				<Link to='/'><FaHome/></Link>
-			</h1>
-			
+		<div className='p-3'>
+			<Link to="/">
+				<FaHome className="text-3xl cursor-pointer text-white-500 relative"/> 
+			</Link>
 		</div>
 	);
 };
